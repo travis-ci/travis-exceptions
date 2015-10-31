f3243de7535ed131fe2983bb79dc2b85a5c347cc
Sven Fuchs <me@svenfuchs.com>
2015-10-31 16:14:47 +0100

    port sidekiq middleware

---
+++ lib/travis/exceptions/sidekiq.rb
@@ -0,0 +1,24 @@
+require 'raven'
+
+module Travis
+  module Exceptions
+    class Sidekiq < Struct.new(:env, :logger)
+      def call(worker, message, queue)
+        yield
+      rescue Exception => error
+        handle(error, queue: queue, worker: worker.to_s, env: env) if retried?(message)
+        raise
+      end
+
+      private
+
+        def retried?(message)
+          message['retry_count'].to_i >= 1
+        end
+
+        def handle(error, extra)
+          Raven.capture_exception(error, extra: extra)
+        end
+    end
+  end
+end
